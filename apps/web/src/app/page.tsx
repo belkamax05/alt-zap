f82b3d02fe853d89b69238a377c7211b084b33a7
Maksym Bondarchuk <maksym.bondarchuk@wundermanthompson.com>
2024-07-26 01:10:59 +0100

    replace to nx

---
+++ apps/web/src/app/page.tsx
@@ -0,0 +1,17 @@
+// import styled from '@emotion/styled';
+
+// const StyledPage = styled.div`
+//   .page {
+//   }
+// `;
+
+export default function Index() {
+  /*
+   * Replace the elements below with your own.
+   *
+   * Note: The corresponding styles are in the ./index.@emotion/styled file.
+   */
+  return (
+    <div className="wrapper">Index page. This page is under development</div>
+  );
+}
