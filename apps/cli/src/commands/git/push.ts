0aa81a462e2a338248d1e9ebc5bedcd3d169e0fc
Maksym Bondarchuk <maksym.bondarchuk@wundermanthompson.com>
2024-08-06 04:06:39 +0100

    latest

--- apps/cli/src/commands/git/push.ts
+++ apps/cli/src/commands/git/push.ts
@@ -1,7 +1,29 @@
+import { confirm, log } from '@clack/prompts';
+import colors from 'colors';
 import Command from '../../types/app/Command';
+import execAsync from '../../utils/exec/execAsync';
+import getBranchDescription from '../../utils/git/getBranchDescription';
 
-const push: Command = () => {
-  console.log('TODO git push with dialogs');
+const push: Command = async () => {
+  const { stdout: commits } = await execAsync(
+    'git log --oneline --branches=$(git rev-parse --abbrev-ref HEAD) --not --remotes',
+  );
+  if (!commits) {
+    const { name } = await getBranchDescription();
+    log.info(`No commits to push at branch '${colors.yellow(name)}'`);
+    return;
+  }
+  log.info('Commits to be pushed:');
+  log.message(commits);
+  const shouldPush = await confirm({
+    message: 'Do you want to push these commits?',
+    initialValue: true,
+  });
+  if (shouldPush) {
+    const { stdout: output } = await execAsync('git push');
+    console.log({ output });
+    log.message(output);
+  }
 };
 
 export default push;
