e7eb684b4b70730677b668c01861b20eec37a8cc
Maksym Bondarchuk <maksym.bondarchuk@wundermanthompson.com>
2024-08-08 03:19:59 +0100

    latest

--- apps/cli/src/commands/az/install.ts
+++ apps/cli/src/commands/az/install.ts
@@ -2,7 +2,7 @@ import Command from '../../types/app/Command';
 import logCommand from '../../utils/logCommand';
 
 const install: Command = ({ command }) => {
-  logCommand(`azInstallPlugin ${command.args._[0]}`);
+  logCommand(`azInstallPlugin ${command.args._[1]}`);
 };
 
 export default install;
