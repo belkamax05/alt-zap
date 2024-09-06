c35c04e334d2640a0f684c9ea75bde97c22c76b2
Maksym Bondarchuk <maksym.bondarchuk@wundermanthompson.com>
2024-09-06 02:20:54 +0100

    latest

--- install.zsh
+++ install.zsh
@@ -1,3 +1,5 @@
 #? RUN: sh -c "$(curl -fsSL https://raw.githubusercontent.com/belkamax05/alt-zap/master/install.zsh)"
 
 echo "Installing alt-zap..."
+export AZ_DIR2=$(dirname "${(%):-%N}")
+echo "Dir running at: $AZ_DIR2"
